9e91352cfeee11deaab9da375d5a258c6f8953e7
shkolnikovmaximilian-ops <shkolnikovmaximilian@gmail.com>
2026-05-09 18:19:00 -0500

    Goal 2 - Implemented bone visibillity toggle functionality

--- src/project.py
+++ src/project.py
@@ -21,3 +21,57 @@ class MALE_PT_rigui(bpy.types.Panel):
             return (context.active_object.data.get("rig_id") == rig_id)
         except (AttributeError, KeyError, TypeError):
             return False
+
+### The code below will allow you to toggle the visibility of rig bones
+
+    def draw(self, context):
+        layout = self.layout
+        col = layout.column()
+        collection = bpy.data.armatures["Armature"].collections
+
+        row = col.row(align = True)  
+        row.prop(collection["PROPERTIES"], 'is_visible', toggle=True, text='PROPERTIES')
+        row = col.row(align = True) 
+        
+        row = col.row(align = True)  
+        row.prop(collection["ROOT"], 'is_visible', toggle=True, text='ROOT')
+        
+        row = col.row(align = True)  
+        row.prop(collection["COG_GUIDE"], 'is_visible', toggle=True, text='COG_GUIDE')
+        
+        row = col.row(align = True)
+        row = col.row(align = True)
+        row = col.row(align = True)  
+        row.prop(collection["TORSO"], 'is_visible', toggle=True, text='TORSO')
+        row.prop(collection["TORSO_FK"], 'is_visible', toggle=True, text='TORSO_FK')
+        row.prop(collection["TORSO_TWEAK"], 'is_visible', toggle=True, text='TORSO_TWEAK')
+        
+        row = col.row(align = True)
+        row = col.row(align = True)
+        row = col.row(align = True) 
+        row.prop(collection["LEFT_ARM_IK"], 'is_visible', toggle=True, text='LEFT_ARM_IK')
+        row.prop(collection["RIGHT_ARM_IK"], 'is_visible', toggle=True, text='RIGHT_ARM_IK')
+        row = col.row(align = True)
+        row.prop(collection["LEFT_ARM_FK"], 'is_visible', toggle=True, text='LEFT_ARM_FK')
+        row.prop(collection["RIGHT_ARM_FK"], 'is_visible', toggle=True, text='RIGHT_ARM_FK')
+        row = col.row(align = True)
+        row.prop(collection["RIGHT_ARM_TWEAK"], 'is_visible', toggle=True, text='RIGHT_ARM_TWEAK')
+        row.prop(collection["LEFT_ARM_TWEAK"], 'is_visible', toggle=True, text='LEFT_ARM_TWEAK')  
+        
+        row = col.row(align = True)
+        row = col.row(align = True)
+        row = col.row(align = True)
+        row.prop(collection["RIGHT_HAND"], 'is_visible', toggle=True, text='RIGHT_HAND')
+        row.prop(collection["LEFT_HAND"], 'is_visible', toggle=True, text='LEFT_HAND') 
+        
+        row = col.row(align = True)
+        row = col.row(align = True)
+        row = col.row(align = True) 
+        row.prop(collection["LEFT_LEG_IK"], 'is_visible', toggle=True, text='LEFT_LEG_IK')
+        row.prop(collection["RIGHT_LEG_IK"], 'is_visible', toggle=True, text='RIGHT_LEG_IK')               
+        row = col.row(align = True)  
+        row.prop(collection["LEFT_LEG_FK"], 'is_visible', toggle=True, text='LEFT_LEG_FK')
+        row.prop(collection["RIGHT_LEG_FK"], 'is_visible', toggle=True, text='RIGHT_LEG_FK')
+        row = col.row(align = True)
+        row.prop(collection["LEFT_LEG_TWEAK"], 'is_visible', toggle=True, text='LEFT_LEG_TWEAK')
+        row.prop(collection["RIGHT_LEG_TWEAK"], 'is_visible', toggle=True, text='RIGHT_LEG_TWEAK') 
